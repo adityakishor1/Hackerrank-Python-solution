b9a8d58041e828465e00f0f46b863ed3f22e9cca
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-05-20 00:11:16 +0530

    Add files via upload

---
+++ DefaultDict.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from collections import defaultdict
+
+d = defaultdict(list)
+n, m = map(int, input().split())
+for i in range(n):
+    d[input()].append(str(i + 1))
+for j in range(m):
+    print(' '.join(d[input()]) or -1)
