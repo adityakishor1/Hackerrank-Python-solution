b33c0d2e24b88b762a8962241bb2b67dc98d8011
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-05-23 15:58:22 +0530

    Add files via upload

---
+++ No idea.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == "__main__":
+    happiness = 0
+    n, m = map(int, input().strip().split(' '))
+    arr = list(map(int, input().strip().split(' ')))
+    
+    good = set(map(int, input().strip().split(' ')))
+    bad = set(map(int, input().strip().split(' ')))
+    
+    for i in arr:
+        if i in good:
+            happiness += 1
+        elif i in bad:
+            happiness -= 1
+    print(happiness)
