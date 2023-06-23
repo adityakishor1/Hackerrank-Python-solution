c46e65fb3764ae6526d8b9c216c07a6df4e29452
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-06-23 23:05:37 +0530

    Add files via upload

---
+++ Check Status.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+for i in range(int(input())):
+    a = int(input())
+    set_a = set(map(int, input().split()))
+
+    b = int(input())
+    set_b = set(map(int, input().split()))
+
+    if len(set_a - set_b) == 0:
+        print("True")
+    else:
+        print("False")
