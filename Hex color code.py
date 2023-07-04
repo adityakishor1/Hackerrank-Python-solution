5684c58735966f4d73f26ec26d6af747857f72c2
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-07-04 10:45:47 +0530

    Add files via upload

---
+++ Hex color code.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+for _ in range(int(input())):
+    matches = re.findall(r':?.(#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3})', input())
+    if matches:
+        print(*matches, sep='\n')
