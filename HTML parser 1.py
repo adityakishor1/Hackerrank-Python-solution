26bbc8951ef49de2f34d61d6efe0d3e700566d9d
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-07-05 19:47:32 +0530

    Add files via upload

---
+++ HTML parser 1.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print ('Start :', tag)
+        for ele in attrs:
+            print ('->', ele[0], '>', ele[1])
+    def handle_endtag(self, tag):
+        print ('End   :', tag)
+    def handle_startendtag(self, tag, attrs):
+        print ('Empty :', tag)
+        for ele in attrs:
+            print ('->', ele[0], '>', ele[1])
+parser = MyHTMLParser()
+for _ in range(int(input())):
+    parser.feed(input())
---
+++ Validating UID.py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+for _ in range(int(input())):
+    u = ''.join(sorted(input()))
+    try:
+        assert re.search(r'[A-Z]{2}', u)
+        assert re.search(r'\d\d\d', u)
+        assert not re.search(r'[^a-zA-Z0-9]', u)
+        assert not re.search(r'(.)\1', u)
+        assert len(u) == 10
+    except:
+        print('Invalid')
+    else:
+        print('Valid')
