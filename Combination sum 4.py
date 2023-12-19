c25b3191f3a11b40be97798ff8ff47ad2212a52f
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-12-19 15:45:03 +0530

    Add files via upload

---
+++ Combination sum 4.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def combinationSum4(self, nums, target):
+        def helper(t):
+            if t<0: return 0
+            if dp[t]>=0: return dp[t]
+            
+            ans = 0
+            for num in nums:
+                ans += helper(t-num)
+            
+            dp[t] = ans
+            return ans
+        
+        dp = [-1]*(target+1)
+        dp[0] = 1
+        
+        for i in xrange(target+1):
+            helper(i)
+        return dp[target]
