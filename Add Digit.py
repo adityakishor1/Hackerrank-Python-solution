0c43ea8012778593d3f92aa9b0dfa9f103e4a235
Aditya kishor <115717863+adityakishor1@users.noreply.github.com>
2023-12-27 17:50:03 +0530

    Add files via upload

---
+++ Add Digit.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addDigits(self, num):
+        if num<10: return num
+        total = 0
+        while num>9:
+            total = 0
+            for d in str(num):
+                total+=int(d)
+            num = total
+        return num
